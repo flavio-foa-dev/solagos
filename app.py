85e48b1849fa5a2eac4abd14cc9e79cf55c89fa3
Flavio Andrade <foa12381@gmail.com>
2024-11-25 21:20:21 -0300

    add css

--- app.py
+++ app.py
@@ -25,7 +25,12 @@ st.markdown(
     """
     <style>
     .st-emotion-cache-1wbqy5l {display: none;}  /* Seleciona e oculta o nome do deploy */
-
+    .css-18e3th9 {
+            visibility: hidden;
+        }
+    .css-1f1i60u {
+        visibility: hidden;
+        }
     </style>
     """,
     unsafe_allow_html=True,
