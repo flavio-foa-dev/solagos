3a4f3f3f5708e22e909d44af3ac96f4fdc767211
Flavio Andrade <foa12381@gmail.com>
2024-11-29 20:08:42 -0300

    css

--- app.py
+++ app.py
@@ -25,7 +25,9 @@ st.markdown(
         <style>
             .st-emotion-cache-1wbqy5l {display: none;}
             .stToolbarActionButton {display: none;}
-            ._link_gzau3_10 {display: none;}
+            ._link_gzau3_10 {
+                background-color: transparent;
+            }
             ._profilePreview_gzau3_63 {display: none;}
             .css-18e3th9 {
                 visibility: hidden;
